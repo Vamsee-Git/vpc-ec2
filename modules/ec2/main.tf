1820fdc5bf3117adbdad1257e0cd5e4d6494a8ba
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-04 08:33:38 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,22 +1,3 @@
-variable "subnet_id" {
-  description = "The ID of the subnet"
-  type        = string
-}
-
-variable "security_group_id" {
-  description = "The ID of the security group"
-  type        = string
-}
-
- name of the key pair"
-  type        = string
-}
-
-variable "ami_id" {
-  description = "The AMI ID"
-  type        = string
-}
-
 resource "aws_instance" "main" {
   ami           = var.ami_id
   instance_type = "t2.micro"
