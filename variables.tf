bdea33dc139d4f0bd5fecfe96aabcb4a4691f8fa
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-04 08:09:44 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "key_name" {
+  description = "The name of the key pair to use for the EC2 instance"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
+}
