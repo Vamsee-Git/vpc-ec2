8e24451211abd303e119d838ffb72e212a2bdbf3
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-04 08:08:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,51 +1,20 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "main-vpc"
-  }
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "main-subnet"
-  }
+module "vpc" {
+  source = "./modules/vpc"
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main-igw"
-  }
+module "security_group" {
+  source  = "./modules/security_group"
+  vpc_id  = module.vpc.vpc_id
 }
 
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "main-route-table"
-  }
-}
-
-resource "aws_route_table_association" "main" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
-}
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.main.id
+module "ec2" {
+  source              = "./modules/ec2"
+  subnet_id           = module.vpc.subnet_id
+  security_group_id   = module.security_group.security_group_id
+  key_name            = var.key_name
+  ami_id              = var.ami_id
 }
