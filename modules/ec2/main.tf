d553df15cdfc27784b78e1fc0082610a6b0229bf
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-04 14:57:12 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "main" {
   ami           = var.ami_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   subnet_id     = var.subnet_id
   key_name      = var.key_name
 
