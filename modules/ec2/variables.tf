958d0c116f0431c416877928761ac78a06323836
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-04 08:34:05 +0530

    Create variables.tf

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "subnet_id" {
+  description = "The ID of the subnet"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "The ID of the security group"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "The AMI ID"
+  type        = string
+}
