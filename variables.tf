78d665e1cf35018a23aa7684e8b7bcf719634d75
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-04 08:42:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "key_name" {
   description = "The name of the key pair to use for the EC2 instance"
   type        = string
+  default     = "MyKeyPair"
 }
 
 variable "ami_id" {
   description = "The AMI ID to use for the EC2 instance"
   type        = string
+  default     = "ami-0e35ddab05955cf57"
 }
