ab6ee62b8a93113f2becc577956c934c549d7e65
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-04 08:15:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ module "vpc" {
 }
 
 module "security_group" {
-  source  = "./modules/security_group"
+  source  = "./modules/security-groups"
   vpc_id  = module.vpc.vpc_id
 }
 
 module "ec2" {
   source              = "./modules/ec2"
   subnet_id           = module.vpc.subnet_id
-  security_group_id   = module.security_group.security_group_id
+  security_group_id   = module.security-group.security_group_id
   key_name            = var.key_name
   ami_id              = var.ami_id
 }
