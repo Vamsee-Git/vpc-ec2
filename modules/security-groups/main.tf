13b3e7d47064f073e572274854dd3039fe703786
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-04 08:13:32 +0530

    Create main.tf

---
+++ modules/security-groups/main.tf
@@ -0,0 +1,30 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+resource "aws_security_group" "main" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "main-sg"
+  }
+}
+
+output "security_group_id" {
+  value = aws_security_group.main.id
+}
