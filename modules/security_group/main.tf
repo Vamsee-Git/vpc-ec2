eaaa937a387625deb68dff38fa1722b982b75155
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-15 05:23:24 +0530

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -7,9 +7,9 @@ resource "aws_security_group" "main" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = ""
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -22,6 +22,23 @@ resource "aws_security_group" "main" {
 
   tags = {
     Name = "main-sg"
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
