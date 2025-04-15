a5afa5d0a034fa4f0c09f55cb8e689e45dd2e1e7
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-15 08:35:03 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,24 +1,26 @@
-variable "subnet_id" {
-  description = "The ID of the subnet"
+variable "ami_id" {
+  description = "The ID of the AMI to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of instance to use"
+  type        = string
+}
+
+
+variable "subnet_ids" {
+  description = "A list of subnet IDs"
+  type        = list(string)
+}
+
+
+variable "key_name" {
+  description = "The name of the SSH key pair"
   type        = string
 }
 
 variable "security_group_id" {
   description = "The ID of the security group"
-  type        = string
-}
-
-variable "ami_id" {
-  description = "The AMI ID"
-  type        = string
-}
-
-variable "key_name" {
-  description = "The name of the key pair to use for the EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "Instance type "
   type        = string
 }
