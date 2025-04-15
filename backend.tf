3788b31e2a9f24dfe06f14d7f8fc2dd8cb17fc4a
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-15 07:45:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform{
   backend "s3" {
     bucket         = "terraform-state-backend-vamsee"
     key            = "terraform/openproject"
-    region         = "ap-south-1"
+    region         = "us-west-1"
     encrypt        = true
   }
 }
