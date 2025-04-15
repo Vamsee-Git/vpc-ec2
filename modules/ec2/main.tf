4ab336a09487528735ef99642a80d6e00a17c635
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-15 08:44:18 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-1"
 }
 
 resource "aws_instance" "main" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = var.subnet_ids[0]
+  subnet_id     = var.subnet_id
   key_name      = var.key_name
   vpc_security_group_ids = [var.security_group_id]
 
