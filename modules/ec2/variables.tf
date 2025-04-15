44fe4df85c79335761b9581fc34e1eb275ed11f1
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-15 08:43:27 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -24,3 +24,10 @@ variable "security_group_id" {
   description = "The ID of the security group"
   type        = string
 }
+
+
+variable "subnet_id" {
+  description = "The ID of the subnet to launch the instance in"
+  type        = string
+}
+
