3b4f35e3475f76493e60251ea80f59c06bbb0d8e
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-15 09:21:14 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,15 +7,18 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count = 3
+  count = 2
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
 
   tags = {
     Name = "PublicSubnet-${count.index}"
   }
 }
+
+data "aws_availability_zones" "available" {}
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
