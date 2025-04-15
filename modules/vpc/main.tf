9248d42e1102c5ba45083ad27c7fba7a81a15711
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-15 09:13:39 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,56 +2,56 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main-vpc"
+    Name = "MainVPC"
   }
 }
 
-resource "aws_subnet" "main" {
-  count = 2
+resource "aws_subnet" "public" {
+  count = 3
   vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   map_public_ip_on_launch = true
 
-  
   tags = {
-    Name = "PublicSubnet-${count.index}"
-  }
-
+    Name = "PublicSubnet-${count.index}"
+  }
 }
 
-resource "aws_internet_gateway" "main" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main-igw"
+    Name = "MainIGW"
   }
 }
 
-resource "aws_route_table" "main" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Name = "main-route-table"
+    Name = "PublicRouteTable"
   }
 }
 
-
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.main[*].id)
-  subnet_id      = element(aws_subnet.main[*].id, count.index)
-  route_table_id = aws_route_table.main.id
+  count          = length(aws_subnet.public[*].id)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
 }
-
 
 output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "subnet_id" {
-  value = aws_subnet.main.id
+output "subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "route_table_id" {
+  value = aws_route_table.public.id
 }
