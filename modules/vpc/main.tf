fd5d6542ae80b373a89b47a75a1931205a1adeb1
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-15 08:11:25 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,13 +7,16 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "main" {
+  count = 2
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   map_public_ip_on_launch = true
 
+  
   tags = {
-    Name = "main-subnet"
-  }
+    Name = "PublicSubnet-${count.index}"
+  }
+
 }
 
 resource "aws_internet_gateway" "main" {
@@ -37,10 +40,13 @@ resource "aws_route_table" "main" {
   }
 }
 
-resource "aws_route_table_association" "main" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.main[*].id)
+  subnet_id      = element(aws_subnet.main[*].id, count.index)
+  route_table_id = aws_route_table.main.id
 }
+
 
 output "vpc_id" {
   value = aws_vpc.main.id
