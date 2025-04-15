ce86b1261c019d5379e6139f462bccb432dad807
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-15 08:22:29 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,14 +1,20 @@
-variable "subnet_id" {
-  description = "The ID of the subnet"
+variable "vpc_id" {
+  description = "The ID of the VPC"
   type        = string
 }
 
-variable "lb_security_group_id" {
-  description = "The ID of the alb"
+variable "subnet_ids" {
+  description = "A list of subnet IDs"
+  type        = list(string)
+}
+
+variable "alb_security_group_id" {
+  description = "The security group ID for the ALB"
   type        = string
 }
 
 variable "instance_id" {
-  description = "The ID of the instance"
+  description = "The ID of the EC2 instance"
   type        = string
 }
+
